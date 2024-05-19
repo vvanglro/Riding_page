5c443789e4a27d31bb6ad42a667413201d0c044f
vvanglro <vvanglro@gmail.com>
2024-05-19 13:24:01 +0800

    1

--- src/static/site-metadata.ts
+++ src/static/site-metadata.ts
@@ -11,19 +11,19 @@ interface ISiteMetadataResult {
 }
 
 const data: ISiteMetadataResult = {
-  siteTitle: 'Workouts Map',
-  siteUrl: 'https://ben29.xyz',
-  logo: 'https://avatars3.githubusercontent.com/u/6956444?s=460&u=97e1062227c4088db3c987146455245c6fa78441',
+  siteTitle: 'Riding Page',
+  siteUrl: 'https://github.com/vvanglro',
+  logo: 'https://avatars.githubusercontent.com/u/43594924?v=4',
   description: 'Personal site and blog',
   keywords: 'workouts, running, cycling, riding, roadtrip, hiking, swimming',
   navLinks: [
     {
       name: 'Blog',
-      url: 'https://ben29.xyz',
+      url: 'https://github.com/vvanglro',
     },
     {
       name: 'About',
-      url: 'https://github.com/ben-29/workouts_page/blob/master/README-CN.md',
+      url: 'https://github.com/vvanglro',
     },
   ],
 };
